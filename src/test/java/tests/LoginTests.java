c9a20c62c3bfbb739e6ae4ed3ff0953969407dd8
AnnaB41 <159689092+AnnaB41@users.noreply.github.com>
2024-03-24 23:29:00 +0200

    Architecture SingleTone registration and login

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,19 +1,10 @@
 package tests;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
-//    WebDriver wd;
-//
-//    @BeforeMethod
-//    public void init(){
-//        wd = new ChromeDriver();
-//        wd.navigate().to("https://telranedu.web.app/home");
-//        wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-//    }
 
     @Test
     public void loginPositiveTest(){
@@ -29,76 +20,30 @@ public class LoginTests extends TestBase{
         Assert.assertTrue(app.getHelperUser().isElementPresent(By.tagName("button")));
 
     }
-//    @Test
-//    public void loginNegativeTestWrongEmail(){
-//        // open login form, find by text
-//        wd.findElement(By.xpath("//*[.='LOGIN']")).click();
-//        //fill login form
-//        WebElement emailInput = wd.findElement(By.xpath("//input[1]")); // создали здесь переменную, потому
-//        // что будем к ней обращаться несколько раз
-//        emailInput.click();
-//        emailInput.clear(); // очищаем форму логина
-//        emailInput.sendKeys("myemail5mail.com"); // вставляем адрес
-//
-//        WebElement passInput = wd.findElement(By.xpath("//input[2]")); // создали здесь переменную, потому
-//        // что будем к ней обращаться несколько раз
-//        passInput.click();
-//        passInput.clear(); // очищаем форму логина
-//        passInput.sendKeys("Tt123456$"); // вставляем пароль
-//
-//        // click on button login
-//        wd.findElement(By.xpath("//button[1]")).click();
-//        Assert.assertTrue(isAlertPresent());
-//        pause(3000);
-//
-//    }
-//    @Test
-//    public void loginNegativeTestWrongPassword(){
-//        // open login form, find by text
-//        wd.findElement(By.xpath("//*[.='LOGIN']")).click();
-//        //fill login form
-//        WebElement emailInput = wd.findElement(By.xpath("//input[1]")); // создали здесь переменную, потому
-//        // что будем к ней обращаться несколько раз
-//        emailInput.click();
-//        emailInput.clear(); // очищаем форму логина
-//        emailInput.sendKeys("myemail5@mail.com"); // вставляем адрес
-//
-//        WebElement passInput = wd.findElement(By.xpath("//input[2]")); // создали здесь переменную, потому
-//        // что будем к ней обращаться несколько раз
-//        passInput.click();
-//        passInput.clear(); // очищаем форму логина
-//        passInput.sendKeys("Tt123456"); // вставляем пароль
-//
-//        // click on button login
-//        wd.findElement(By.xpath("//button[1]")).click();
-//        // assert пока нет
-//        pause(3000);
-//
-//    }
-//
-//    @AfterMethod
-//    public void tearDown(){
-//        pause(3000);
-//        wd.quit();
-//    }
+    @Test
+    public void loginNegativeTestWrongEmail(){
+        // open login form, find by text
+       app.getHelperUser().openLoginRegistrationForm();
+        //fill login form
+       app.getHelperUser().fillLoginRegistrationForm("myemail5mail.com", "Tt123456$");
+        // click on button login
+        app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isAlertPresent());
+        app.getHelperUser().pause(3000);
 
+    }
+    @Test
+    public void loginNegativeTestWrongPassword(){
+        // open login form, find by text
+        app.getHelperUser().openLoginRegistrationForm();
+        //fill login form
+       app.getHelperUser().fillLoginRegistrationForm("myemail5@mail.com","Tt123456" );
+       // click on button login
+        app.getHelperUser().submitLogin();
+        // assert пока нет
+        Assert.assertTrue(app.getHelperUser().isAlertPresent());
+        app.getHelperUser().pause(3000);
 
-//    public void pause(int millis){
-//        try {
-//            Thread.sleep(millis);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public boolean isAlertPresent(){
-//        Alert alert = new WebDriverWait(wd, Duration.ofSeconds(3))
-//                .until(ExpectedConditions.alertIsPresent()); //ждем, присутствует ли окошко Алерта на браузере
-//        if(alert == null) return false;
-//        wd.switchTo().alert();
-//        System.out.println(alert.getText());
-//        alert.accept();
-//        return true;
-//    }
+    }
 
 }
--- src/test/java/tests/RegistrationTest.java
+++ src/test/java/tests/RegistrationTest.java
@@ -6,109 +6,60 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class RegistrationTest extends TestBase{
-//
-//    @Test
-//    public void registrationPositiveTest(){
-//        // open login form, find by text
-//        wd.findElement(By.xpath("//*[.='LOGIN']")).click();
-//        //fill login form
-//        int i = (int)(System.currentTimeMillis()/1000)%3600; // получаем рандомное число, приводим в инт
-//        WebElement emailInput = wd.findElement(By.xpath("//input[1]")); // создали здесь переменную, потому
-//        // что будем к ней обращаться несколько раз
-//        emailInput.click();
-//        emailInput.clear(); // очищаем форму логина
-//        emailInput.sendKeys("mye" + i +"@mail.com"); // вставляем адрес с рандомным числом
-//
-//        WebElement passInput = wd.findElement(By.xpath("//input[2]")); // создали здесь переменную, потому
-//        // что будем к ней обращаться несколько раз
-//        passInput.click();
-//        passInput.clear(); // очищаем форму логина
-//        passInput.sendKeys("Tt123456$"); // вставляем пароль
-//
-//        // click on button registration
-//        wd.findElement(By.xpath("//button[2]")).click();
-//        // assert ищем коллекцию по Баттон (1 кнопка) и ее размер. Если кнопка есть, то размер коллекции меньше 1
-//        //   pause(3000);
-//        Assert.assertTrue(wd.findElements(By.tagName("button")).size() > 0);
-//    }
-//
-//    @Test
-//    public void registrationNegativeTestWrongEmail(){
-//        // open login form, find by text
-//        wd.findElement(By.xpath("//*[.='LOGIN']")).click();
-//        //fill login form
-//        int i = (int)(System.currentTimeMillis()/1000)%3600; // получаем рандомное число, приводим в инт 4 цифры
-//        WebElement emailInput = wd.findElement(By.xpath("//input[1]")); // создали здесь переменную, потому
-//        // что будем к ней обращаться несколько раз
-//        emailInput.click();
-//        emailInput.clear(); // очищаем форму логина
-//        emailInput.sendKeys("mye" + i +"mail.com"); // вставляем адрес с рандомным числом
-//
-//        WebElement passInput = wd.findElement(By.xpath("//input[2]")); // создали здесь переменную, потому
-//        // что будем к ней обращаться несколько раз
-//        passInput.click();
-//        passInput.clear(); // очищаем форму логина
-//        passInput.sendKeys("Tt123456$"); // вставляем пароль
-//
-//        // click on button registration
-//        wd.findElement(By.xpath("//button[2]")).click();
-//        Assert.assertTrue(isAlertPresent());
-//        //   pause(3000);
-//
-//    }
-//
-//    @Test
-//    public void registrationNegativeTestWrongPassword(){
-//        // open login form, find by text
-//        wd.findElement(By.xpath("//*[.='LOGIN']")).click();
-//        //fill login form
-//        int i = (int)(System.currentTimeMillis()/1000)%3600; // получаем рандомное число, приводим в инт
-//        WebElement emailInput = wd.findElement(By.xpath("//input[1]")); // создали здесь переменную, потому
-//        // что будем к ней обращаться несколько раз
-//        emailInput.click();
-//        emailInput.clear(); // очищаем форму логина
-//        emailInput.sendKeys("mye" + i +"@mail.com"); // вставляем адрес с рандомным числом
-//
-//        WebElement passInput = wd.findElement(By.xpath("//input[2]")); // создали здесь переменную, потому
-//        // что будем к ней обращаться несколько раз
-//        passInput.click();
-//        passInput.clear(); // очищаем форму логина
-//        passInput.sendKeys("Tt123456"); // вставляем пароль
-//
-//        // click on button registration
-//        wd.findElement(By.xpath("//button[2]")).click();
-//        // assert ищем коллекцию по Баттон (1 кнопка) и ее размер. Если кнопка есть, то размер коллекции меньше 1
-//        //   pause(3000);
-//
-//    }
-//
-//    @Test
-//    public void registrationNegativeTestUserExist(){
-//        // open login form, find by text
-//        wd.findElement(By.xpath("//*[.='LOGIN']")).click();
-//        //fill login form
-//        WebElement emailInput = wd.findElement(By.xpath("//input[1]")); // создали здесь переменную, потому
-//        // что будем к ней обращаться несколько раз
-//        emailInput.click();
-//        emailInput.clear(); // очищаем форму логина
-//        emailInput.sendKeys("myemail5@mail.com"); // вставляем адрес с рандомным числом
-//
-//        WebElement passInput = wd.findElement(By.xpath("//input[2]")); // создали здесь переменную, потому
-//        // что будем к ней обращаться несколько раз
-//        passInput.click();
-//        passInput.clear(); // очищаем форму логина
-//        passInput.sendKeys("Tt123456$"); // вставляем пароль
-//
-//        // click on button registration
-//        wd.findElement(By.xpath("//button[2]")).click();
-//        // assert ищем коллекцию по Баттон (1 кнопка) и ее размер. Если кнопка есть, то размер коллекции меньше 1
-//        //   pause(3000);
-//
-//    }
-//
 
+    @Test
+    public void registrationPositiveTest(){
+        // open login form, find by text
+        app.getHelperUser().openLoginRegistrationForm();
+        //fill login form
+        int i = (int)(System.currentTimeMillis()/1000)%3600; // получаем рандомное число, приводим в инт
+        app.getHelperUser().fillLoginRegistrationForm("mye" + i +"@mail.com", "Tt123456$");
+       // click on button registration
+       app.getHelperUser().submitRegistration();
+        // assert ищем коллекцию по Баттон (1 кнопка) и ее размер. Если кнопка есть, то размер коллекции меньше 1
+       app.getHelperUser().pause(3000);
+        Assert.assertTrue(app.getHelperUser().isElementPresent(By.tagName("button")));
 
+    }
 
-        }
+    @Test
+    public void registrationNegativeTestWrongEmail(){
+        // open login form, find by text
+       app.getHelperUser().openLoginRegistrationForm();
+        //fill login form
+        int i = (int)(System.currentTimeMillis()/1000)%3600; // получаем рандомное число, приводим в инт 4 цифры
+        app.getHelperUser().fillLoginRegistrationForm("mye" + i +"mail.com", "Tt123456$");
+        // click on button registration
+        app.getHelperUser().submitRegistration();
+        Assert.assertTrue(app.getHelperUser().isAlertPresent());
+        app.getHelperUser().pause(3000);
+
+    }
+
+    @Test
+    public void registrationNegativeTestWrongPassword(){
+        // open login form, find by text
+        app.getHelperUser().openLoginRegistrationForm();
+        //fill login form
+        int i = (int)(System.currentTimeMillis()/1000)%3600; // получаем рандомное число, приводим в инт 4 цифры
+        app.getHelperUser().fillLoginRegistrationForm("mye" + i +"@mail.com", "Tt123456");
+        // click on button registration
+        app.getHelperUser().submitRegistration();
+        Assert.assertTrue(app.getHelperUser().isAlertPresent());
+        app.getHelperUser().pause(3000);
+    }
+
+    @Test
+    public void registrationNegativeTestUserExist(){
+        // open login form, find by text
+        app.getHelperUser().openLoginRegistrationForm();
+        //fill login form
+        app.getHelperUser().fillLoginRegistrationForm("myemail5@mail.com", "Tt123456$");
+        // click on button registration
+        app.getHelperUser().submitRegistration();
+        Assert.assertTrue(app.getHelperUser().isAlertPresent());
+        app.getHelperUser().pause(3000);
+    }
+}
 
 
